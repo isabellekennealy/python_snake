c0c9cc8a783aa1b7b57c74f03a848a14feea7aca
Isabelle Kennealy <isabellekennealy@Isabelles-MacBook-Pro.local>
2022-03-29 18:07:32 -0400

    created basic game skeleton

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Classic snake game in oython. Code was generated following a tutorial
+'''
+
+# import required packages
+import pygame
+
+# initialize the game
+pygame.init()
+
+# set the screen size
+dis=pygame.display.set_mode((400,300))
+
+#draw the screen
+pygame.display.update()
+
+# quit the game
+pygame.quit()
+quit()
